30e4dd2ede9d1252b3f4a24ba60a9d53e5360d99
Kim SAVAROCHE <kimsavinfo@gmail.com>
2021-08-18 17:24:08 +0200

    use imported tools

--- MyProject/main.cpp
+++ MyProject/main.cpp
@@ -6,10 +6,19 @@
 //
 
 #include <iostream>
+#include "FizzTools.hpp"
+#include "BuzzTools.hpp"
 
 int main(int argc, const char * argv[]) {
     for(int iNumber = 1; iNumber <= 100; iNumber++) {
         std::string text = "";
+        
+        if( FizzTools::shouldFizz(iNumber) ) {
+            text += "fizz";
+        }
+        if ( BuzzTools::shouldBuzz(iNumber) ) {
+            text += "buzz";
+        }
         
         if(text.size() == 0) {
             text = std::to_string(iNumber);
